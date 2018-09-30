b212bcc7a8c395752bd890076c02418911848c2a
Evan Mattson <me@aaemnnost.tv>
2018-09-30 19:00:10 +0300

    connect vue-devtools in dev

--- src/switcher.js
+++ src/switcher.js
@@ -1,8 +1,20 @@
+import devtools from '@vue/devtools'
 import Vue from 'vue'
 import Switcher from 'components/Switcher.vue'
 
-/* eslint-disable no-new */
-window.switcher = new Vue({
+// eslint-disable-next-line no-undef
+if (process.env.NODE_ENV === 'development') {
+  try {
+    devtools.connect()
+  } catch (e) {
+    console.log(
+      'Vue Devtools not connected. Make sure remote devtools are running.'
+    )
+  }
+}
+
+/* eslint-disable-next-line no-new */
+new Vue({
   el: '#switcher',
   render: h => h(Switcher)
 })
