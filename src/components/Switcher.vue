166acdda334f9e8df6445a4051d257b849fde4fd
Evan Mattson <me@aaemnnost.tv>
2018-09-30 17:48:39 +0300

    refactor switcher

--- src/components/Switcher.vue
+++ src/components/Switcher.vue
@@ -1,11 +1,11 @@
 <template>
   <div id="switcher"
     @keydown.enter="switchToHighlighted"
-    @keydown.up="prevTab"
-    @keydown.ctrl.k="prevTab"
-    @keydown.down="nextTab"
-    @keydown.ctrl.j="nextTab"
-    @keydown.ctrl.x="removeHighlighted"
+    @keydown.up="highlightPreviousTab"
+    @keydown.ctrl.k="highlightPreviousTab"
+    @keydown.down="highlightNextTab"
+    @keydown.ctrl.j="highlightNextTab"
+    @keydown.ctrl.x="removeHighlightedTab"
     @keydown.ctrl.f="filterInput = ''"
     @keydown.esc="close"
   >
@@ -16,6 +16,19 @@
       >
     </div>
 
+    <div class="tab-list">
+      <tab v-for="tab in filteredTabs" :key="tab.id"
+        :id="tab.id"
+        :url="tab.url"
+        :title="tab.title"
+        :incognito="tab.incognito"
+        :fav-icon-url="tab.favIconUrl"
+        :highlighted="tab.id === highlightedTabId"
+        @highlight="highlightTab(tab.id)"
+        @switchTo="switchToTab(tab.id)"
+        @close="removeTab(tab.id)"
+      ></tab>
+    </div>
   </div>
 </template>
 
@@ -24,38 +37,73 @@ import browser from 'webextension-polyfill'
 import filter from 'lodash/filter'
 import pick from 'lodash/pick'
 import findIndex from 'lodash/findIndex'
+import Tab from './Tab.vue'
 import fuzzyFilter from '../fuzzy-matcher'
 import fuzzyTransform from '../fuzzy-results-transformer'
 
 export default {
   data() {
     return {
+      highlightedTabId: null,
       tabId: null,
       tabs: [],
       filterInput: ''
     }
   },
   components: {
+    Tab
   },
   created() {
-    browser.runtime.sendMessage({ action: 'getTabs' }).then(tabs => this.$set(this, 'tabs', tabs))
-
+    browser.runtime
+      .sendMessage({ action: 'getTabs' })
+      .then(tabs => this.$set(this, 'tabs', tabs))
   },
   methods: {
-    switchToTab(tabId) {
-      browser.runtime.sendMessage({ action: 'switchToTab', tabId: tabId }).then(this.close)
+    highlightTab(tabId) {
+      this.highlightedTabId = tabId
     },
-    removeHighlighted() {
+    highlightPreviousTab() {
+      const curIdx = findIndex(this.filteredTabs, { id: this.highlightedTabId })
+      const prevIdx = curIdx - 1
+
+      if (prevIdx > -1) {
+        this.highlightTab(this.filteredTabs[prevIdx].id)
+      } else {
+        this.highlightTab(this.lastTabId)
+      }
+    },
+    highlightNextTab() {
+      const curIdx = findIndex(this.filteredTabs, { id: this.highlightedTabId })
+      const nextIdx = curIdx + 1
+
+      if (nextIdx < this.filteredTabs.length) {
+        this.highlightTab(this.filteredTabs[nextIdx].id)
+      } else {
+        this.highlightTab(this.firstTabId)
+      }
+    },
+    removeHighlightedTab() {
+      const tabId = this.highlightedTabId
+      const tabIdx = findIndex(this.filteredTabs, { id: tabId })
+      const method =
+        this.filteredTabs.length === tabIdx + 1
+          ? 'highlightPreviousTab'
+          : 'highlightNextTab'
+
+      this[method]()
+      this.removeTab(tabId)
+    },
+    switchToTab(tabId) {
+      browser.runtime
+        .sendMessage({ action: 'switchToTab', tabId: tabId })
+        .then(this.close)
     },
     removeTab(tabId) {
       this.$delete(this.tabs, findIndex(this.tabs, { id: tabId }))
       browser.tabs.remove(tabId)
     },
-    prevTab() {
-    },
-    nextTab() {
-    },
     switchToHighlighted() {
+      this.switchToTab(this.highlightedTabId)
     },
     close() {
       window.close()
@@ -66,7 +114,7 @@ export default {
       return this.filterInput.trim()
     },
     filteredTabs() {
-      if (! this.search.length) {
+      if (!this.search.length) {
         return this.tabs
       }
 
@@ -77,11 +125,35 @@ export default {
 
       const fuzzyResults = fuzzyFilter(this.tabs, this.search, { keys })
 
-      return fuzzyTransform(fuzzyResults, '<span class="match">','</span>')
+      return fuzzyTransform(fuzzyResults, '<span class="match">', '</span>')
+    },
+    firstTabId() {
+      if (!this.filteredTabs || !this.filteredTabs.length) {
+        return null
+      }
+
+      return this.filteredTabs[0].id
+    },
+    lastTabId() {
+      if (!this.filteredTabs || !this.filteredTabs.length) {
+        return null
+      }
+
+      return this.filteredTabs[this.filteredTabs.length - 1].id
     }
   },
   mounted() {
     this.$refs.input.focus()
+  },
+  watch: {
+    filteredTabs(value) {
+      if (!this.highlightedTabId && this.firstTabId) {
+        this.highlightTab(this.firstTabId)
+      }
+    },
+    search() {
+      this.highlightTab(this.firstTabId)
+    }
   }
 }
 </script>
